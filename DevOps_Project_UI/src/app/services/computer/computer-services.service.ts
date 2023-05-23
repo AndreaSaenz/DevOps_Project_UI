d4ac5d1d6712c42c1b7c76805f03b300f78f592e
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-23 13:21:32 -0600

    Agregando computer services

--- DevOps_Project_UI/src/app/services/computer/computer-services.service.ts
+++ DevOps_Project_UI/src/app/services/computer/computer-services.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComputerServicesService {
 
-  constructor() { }
+  private URL = 'http://localhost:3000/computers'
+
+  constructor(private http: HttpClient) {}
+
+  getAllComputers(): Observable<any> {
+    return this.http.get(this.URL);
+  }
+
+  getComputerById(id: number): Observable<any> {
+    return this.http.get(this.URL + `/${id}`);
+  }
+
+  createComputer(data: any): Observable<any> {
+    return this.http.post(this.URL, JSON.stringify(data));
+  }
+
+  updateComputer(id: number, data: any): Observable<any> {
+    return this.http.put<any>( this.URL + `/${id}`, JSON.stringify(data));
+  }
+
+  deleteComputer(id: number): Observable<any> {
+    return this.http.delete( this.URL + `/${id}`);
+  }
 }

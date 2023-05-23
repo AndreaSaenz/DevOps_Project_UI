9aeed955e911d2cffaeef1b5ef5eab5e897cab50
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-23 05:51:16 -0600

    Editando componentes de estudiantes

--- DevOps_Project_UI/src/app/components/main-content/students/create-student-component/create-student-component.component.ts
+++ DevOps_Project_UI/src/app/components/main-content/students/create-student-component/create-student-component.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { StudentServicesService } from 'src/app/services/student/student-services.service';
+
 
 @Component({
   selector: 'app-create-student-component',
@@ -7,4 +12,42 @@ import { Component } from '@angular/core';
 })
 export class CreateStudentComponentComponent {
 
+  newStudent: FormGroup;
+
+  constructor(private router: Router, private formBuilder: FormBuilder, private studentService: StudentServicesService){
+    this.newStudent = this.formBuilder.group({
+      Name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, Validators.pattern('.+@.+\.com')]],
+      telefono: ['', Validators.pattern('[0-9]{3}-[0-9]{3}-[0-9]{4}')],
+      licenciatura: [''],
+      semestre: ['', [Validators.min(0), Validators.max(30)]]
+    })
+  }
+
+  createStudent(){
+    
+    const student: any ={
+      "Name": this.newStudent.value.Name,
+      "email": this.newStudent.value.email,
+      "telefono": this.newStudent.value.telefono,
+      "licenciatura": this.newStudent.value.licenciatura,
+      "semestre": this.newStudent.value.semestre
+    }
+    console.log(student);
+    console.log(student.Name);
+    console.log(this.newStudent);
+
+    this.studentService.createStudent(student).subscribe({
+      next: (val: any) => {
+        window.alert('Alumno creado exitosamente');
+        this.router.navigate(['/main/students']);
+      },
+      error: (error: any) => {
+        console.error(error);
+        window.alert(`Error Code: ${error.status}\nMessage: ${error.message}`);
+      },
+    });
+
+  }
+  
 }
--- DevOps_Project_UI/src/app/components/main-content/students/students-table-component/students-table-component.component.ts
+++ DevOps_Project_UI/src/app/components/main-content/students/students-table-component/students-table-component.component.ts
@@ -1,10 +1,151 @@
-import { Component } from '@angular/core';
+import { Component, NgModuleFactory, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, isFormControl, NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { StudentInterface } from 'src/app/interfaces/student-interface';
 
 @Component({
   selector: 'app-students-table-component',
   templateUrl: './students-table-component.component.html',
   styleUrls: ['./students-table-component.component.css']
 })
-export class StudentsTableComponentComponent {
+export class StudentsTableComponentComponent implements OnInit{
+
+  students1 : any[] = []; 
+
+  backup: any = {};
+  id: string | null;
+  title = 'Alumnos';
+
+  students :  Array<StudentInterface>= [
+    {
+      "id": 65,
+      "Name": "Pepe",
+      "email": "Otto@email.com",
+      "telefono":"1818515",
+      "licenciatura": "lis",
+      "semestre": 4,
+      "editMode": false
+    },
+    {
+      "id": 66,
+      "Name": "Jacob",
+      "email": "Thornton@email.com",
+      "telefono":"1818556",
+      "licenciatura": "lic",
+      "semestre": 5,
+      "editMode": false
+    },
+    {
+      "id": 67,
+      "Name": "Larry the Bird",
+      "email": "Larry_the_Bird@email.com",
+      "telefono":"18455556",
+      "licenciatura": "lcc",
+      "semestre": 3,
+      "editMode": false
+    },
+    {
+      "id": 68,
+      "Name": "Larry the Bird",
+      "email": "Larry_the_Bird@email.com",
+      "telefono":"18455556",
+      "licenciatura": "lcc",
+      "semestre": 3,
+      "editMode": false
+    },
+    {
+      "id": 69,
+      "Name": "Larry the Bird",
+      "email": "Larry_the_Bird@email.com",
+      "telefono":"18455556",
+      "licenciatura": "lcc",
+      "semestre": 3,
+      "editMode": false
+    },
+    {
+      "id": 70,
+      "Name": "Larry the Bird",
+      "email": "Larry_the_Bird@email.com",
+      "telefono":"18455556",
+      "licenciatura": "lcc",
+      "semestre": 3,
+      "editMode": false
+    }
+  ]
+
+  oneStudent: Array<StudentInterface> = [{
+      "id": 75,
+      "Name": "Pepe",
+      "email": "Otto@email.com",
+      "telefono":"1818515",
+      "licenciatura": "lis",
+      "semestre": 4,
+      "editMode": false
+  }]
+
+  constructor( private router: Router, private activatedRoute: ActivatedRoute) { 
+    this.id = this.activatedRoute.snapshot.paramMap.get('studentId');
+    console.log(this.id);
+  }
+
+  ngOnInit(): void {
+    this.backup = {
+      "id": 0,
+      "Name": "",
+      "email": "",
+      "telefono":"",
+      "licenciatura": "",
+      "semestre": 0,
+      "editMode": true
+    };
+    this.searchByIdMode();
+  }
+
+  editModeOn(student: any){
+    this.students.forEach(element => {
+      element.editMode = false;
+    });
+    student.editMode = true;
+    Object.assign(this.backup, student);
+  }
+
+  searchStudentById( search: boolean, form: NgForm){
+    console.log(form.valid);
+    console.log(search);
+    console.log(form.value);
+    console.log(Object.values(form.value)[0]);
+    this.router.navigate(['/main/students/', Object.values(form.value)[0]]);
+  }
+
+  updateStudent(form: NgForm){
+    //Invocar método PUT del servicio
+    console.log(Object.values(form.value));
+    console.log(form.value);
+    console.log(Object.entries(form.value));
+    this.students.forEach(element => {
+      element.editMode = false;
+    });
+    this.router.navigate(['/main/students']);
+  }
+
+  cancelEditMode(student: any){
+    Object.assign(student, this.backup );
+    student.editMode = false;
+    this.backup = {};
+  }
+
+  deleteStudent(student: any){
+    //Invocar método DELETE del servicio 
+    //this.router.navigate(['/main/students']);
+  }
+
+  searchByIdMode(){
+    if(this.id !== null){
+      this.title = 'Alumno (' + this.id + ')';
+      this.students = this.oneStudent;
+
+    }
+  }
 
 }

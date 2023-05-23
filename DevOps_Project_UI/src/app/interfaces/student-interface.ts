d2ce3c42897ea2188b95241edb8ac4aa5423ca53
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-23 05:49:12 -0600

    Editando interfaz alumno

--- DevOps_Project_UI/src/app/interfaces/student-interface.ts
+++ DevOps_Project_UI/src/app/interfaces/student-interface.ts
@@ -1,2 +1,9 @@
 export interface StudentInterface {
+    "id": number,
+    "Name": string,
+    "email": string,
+    "telefono": string,
+    "licenciatura": string,
+    "semestre": number,
+    "editMode": boolean 
 }

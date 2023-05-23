5fba54b78ac4e4e4d762ac7411589f23e1b9c6cb
Desiree Correa <audny.correac@gmail.com>
2023-05-23 16:15:54 -0600

    add login and register

--- DevOps_Project_UI/src/app/components/login-component/login-component.component.ts
+++ DevOps_Project_UI/src/app/components/login-component/login-component.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserServicesService } from 'src/app/services/user/user-services.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
-  styleUrls: ['./login-component.component.css']
+  styleUrls: ['./login-component.component.css'],
 })
-export class LoginComponentComponent {
+export class LoginComponentComponent implements OnInit {
+  user: any;
 
+  constructor(private userAuth: UserServicesService, private router: Router) {}
+
+  ngOnInit() {}
+
+  logIn() {
+    this.userAuth.logInUser(this.user).subscribe((res) => {
+      localStorage.setItem('token', res.token);
+      this.router.navigate(['/students']);
+    });
+  }
 }
--- DevOps_Project_UI/src/app/services/user/user-services.service.ts
+++ DevOps_Project_UI/src/app/services/user/user-services.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserServicesService {
+  private URL = 'http://localhost:3000/user';
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor() { }
+  registerNewUser(user: any) {
+    return this.http.post<any>(this.URL + '/register', user);
+  }
+
+  logInUser(user: any) {
+    return this.http.post<any>(this.URL + '/login', user);
+  }
+
+  loggedIn() {
+    return !!localStorage.getItem('token');
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
 }

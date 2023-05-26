9ea1407d9ddef6a3bb517b41d4a534ac3185b0d1
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-25 18:29:57 -0600

    Arreglando errores del servicio de users

--- DevOps_Project_UI/src/app/services/user/user-services.service.ts
+++ DevOps_Project_UI/src/app/services/user/user-services.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserServicesService {
+
   private URL = 'http://localhost:3000/user';
+
   constructor(private http: HttpClient, private router: Router) {}
 
   registerNewUser(user: any) {
-    return this.http.post<any>(this.URL + '/register', user);
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type': 'application/json'})
+    }
+    return this.http.post<any>(this.URL + '/register', JSON.stringify(user), httpOptions);
   }
 
   logInUser(user: any) {
-    return this.http.post<any>(this.URL + '/login', user);
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type': 'application/json'})
+    }
+    return this.http.post<any>(this.URL + '/login', JSON.stringify(user), httpOptions );
   }
 
   loggedIn() {

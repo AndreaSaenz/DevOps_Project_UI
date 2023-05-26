e3886337b923ac1b29c40f5ad26abc6c952020e4
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-26 04:05:11 -0600

    Editando cabeceras y agregando refresh

--- DevOps_Project_UI/src/app/services/student/student-services.service.ts
+++ DevOps_Project_UI/src/app/services/student/student-services.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject, tap  } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,13 @@ export class StudentServicesService {
 
   private URL = 'http://localhost:3000/students'
 
+  private refresh = new Subject<void>();
+
   constructor(private http: HttpClient) {}
+
+  get refreshNeeded(){
+    return this.refresh;
+  }
 
   getAllStudents(): Observable<any> {
     return this.http.get(this.URL);
@@ -24,16 +30,33 @@ export class StudentServicesService {
       headers: new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${localStorage.getItem('token')}` 
-    })
+      })
     }
     return this.http.post(this.URL, JSON.stringify(data), httpOptions);
   }
 
   updateStudent(id: number, data: any): Observable<any> {
-    return this.http.put<any>( this.URL + `/${id}`, JSON.stringify(data));
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}` 
+      })
+    }
+    return this.http.put<any>( this.URL + `/${id}`, JSON.stringify(data), httpOptions)
+    .pipe(
+      tap( () => {
+        this.refresh.next();
+      })
+    );
   }
 
   deleteStudent(id: number): Observable<any> {
-    return this.http.delete( this.URL + `/${id}`);
+    return this.http.delete( this.URL + `/${id}`)
+    .pipe(
+      tap( () => {
+        this.refresh.next();
+      })
+    );
   }
+  
 }

4741e5516650f0b4d7d499af15418bd490eeaa6d
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-25 18:27:55 -0600

    Arreglando errores del componente login

--- DevOps_Project_UI/src/app/components/login-component/login-component.component.ts
+++ DevOps_Project_UI/src/app/components/login-component/login-component.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { UserServicesService } from 'src/app/services/user/user-services.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { UserServicesService } from 'src/app/services/user/user-services.service';
 
 @Component({
   selector: 'app-login-component',
@@ -8,16 +10,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-component.component.css'],
 })
 export class LoginComponentComponent implements OnInit {
-  user: any;
+  user: any = {};
 
   constructor(private userAuth: UserServicesService, private router: Router) {}
 
   ngOnInit() {}
 
-  logIn() {
+  logIn(form: NgForm) {
+    //console.log(form.value);
+    //console.log(this.user);
     this.userAuth.logInUser(this.user).subscribe((res) => {
+      //console.log(this.user);
       localStorage.setItem('token', res.token);
-      this.router.navigate(['/students']);
+      this.router.navigate(['/main/students']);
     });
   }
+  /*
+  logIn2(form: NgForm){
+    console.log(form.value);
+    console.log(this.user);
+    console.log(JSON.stringify(form.value));
+    console.log(JSON.stringify(this.user));
+    this.userAuth.logInUser(form.value).subscribe({
+      next: (val: any) => {
+        console.log(val);
+        console.log(val.token);
+        localStorage.setItem('token', val.token);
+        this.router.navigate(['/main/students']);
+      },
+      error: (error: any) => {
+        console.error(error);
+        window.alert(`Error Code: ${error.status}\nMessage: ${error.message}`);
+      },
+    });
+  }
+  */
 }

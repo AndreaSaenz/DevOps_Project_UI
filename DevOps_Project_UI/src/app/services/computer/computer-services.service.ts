755d4147b82b40f7097837fa101bd3d8a6363473
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-28 20:56:43 -0600

    Arreglando servicios de computers

--- DevOps_Project_UI/src/app/services/computer/computer-services.service.ts
+++ DevOps_Project_UI/src/app/services/computer/computer-services.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Observable, Subject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,13 @@ export class ComputerServicesService {
 
   private URL = 'http://localhost:3000/computers'
 
+  private refresh = new Subject<void>();
+
   constructor(private http: HttpClient) {}
+
+  get refreshNeeded(){
+    return this.refresh;
+  }
 
   getAllComputers(): Observable<any> {
     return this.http.get(this.URL);
@@ -20,14 +26,36 @@ export class ComputerServicesService {
   }
 
   createComputer(data: any): Observable<any> {
-    return this.http.post(this.URL, JSON.stringify(data));
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}` 
+      })
+    }
+    return this.http.post(this.URL, JSON.stringify(data), httpOptions);
   }
 
   updateComputer(id: number, data: any): Observable<any> {
-    return this.http.put<any>( this.URL + `/${id}`, JSON.stringify(data));
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('token')}` 
+      })
+    }
+    return this.http.put<any>( this.URL + `/${id}`, JSON.stringify(data), httpOptions)
+    .pipe(
+      tap( () => {
+        this.refresh.next();
+      })
+    );
   }
 
   deleteComputer(id: number): Observable<any> {
-    return this.http.delete( this.URL + `/${id}`);
+    return this.http.delete( this.URL + `/${id}`)
+    .pipe(
+      tap( () => {
+        this.refresh.next();
+      })
+    );
   }
 }

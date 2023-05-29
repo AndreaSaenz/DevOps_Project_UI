af00c69a0d5ff4b1c3839244daa78bf157cb8cdd
AndreaSaenz <andrea.inteligente1979@gmail.com>
2023-05-29 06:12:05 -0600

    Agregando loan interface

--- DevOps_Project_UI/src/app/interfaces/loan-interface.ts
+++ DevOps_Project_UI/src/app/interfaces/loan-interface.ts
@@ -1,2 +1,9 @@
 export interface LoanInterface {
+    "folio": number, //not null
+    "estado": boolean, //not null
+    "fechaEstipuladaDev": Date, //not null?
+    "fechaRealDev": Date,
+    "observacion": string,
+    "fechaInicio": Date,
+    "editMode": boolean 
 }
